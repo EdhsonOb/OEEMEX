0cc5d6aae80e0e0bc8a108d872e0943da547c896
EdhsonOb <edhson.obed85@gmail.com>
2023-03-20 20:05:54 -0600

    inventarioTerminado

---
+++ archivos/filesAdmin.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../cssv2/files.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+    <link rel="shortcut icon" href="../img/logo1.png" type="image/x-icon">
+    <title>Archivos</title>
+</head>
+<body>
+<nav class="navbar navbar-dark bg-primary">
+        <a class="navbar-brand" href="#">
+          <img style="margin-left: 5%; margin-right: 2%;"src="../img/logo1.png" width="30" height="30" class="d-inline-block align-top" alt="">
+            Panel
+        </a>
+        
+        <ul class="nav justify-content-end">
+            <li class="nav-item class2">
+              <a class="nav-link" style="color: white;" href="../bd/cses.php">Cerrar sesion</a>
+            </li>
+          </ul>
+      </nav>
+
+      </div>
+    <div class="sliderbar">
+    <div id="menu">
+        <ul>
+            <li class="menuz"><a href="#" class="menuv" style="text-decoration: none;" onclick="cargarContenido('filesTadmin.php')">Mis archivos</a></li>
+            <li class="menuz"><a href="#" class="menuv" style="text-decoration: none;" onclick="cargarContenido('usuarios.php')">Usuarios</a></li>
+            <li class="menuz"><a href="#" class="menuv" style="text-decoration: none;" onclick="cargarContenido('inventario.php')">Inventario</a></li>
+        </ul>
+
+    </div>
+
+    </div>
+</body>
+</html>
